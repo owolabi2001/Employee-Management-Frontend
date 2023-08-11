244a95866e621f338431e7fefb3eee14ad721251
owolabi2001 <owolabitemiolamide@gmail.com>
2023-08-11 01:50:13 +0100

    "Added a few change"

--- src/components/ReSubComponent.jsx
+++ src/components/ReSubComponent.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 import "./cssForComponents/reSubComponent.css"
+import axios from 'axios'
+import { useEffect } from 'react'
 
-const ReSubComponent = ({item}) => {
+const ReSubComponent = ({item, BASE_URL}) => {
+  const deleteEmployee = ()=>{
+    axios.delete(BASE_URL + "/deleteEmployee",{
+      params: {
+        name: item.staffName
+      }
+    }).then((response)=>{
+      alert(response.data.message)
+    }).catch(
+      (e)=>{
+        console.log("Problem")
+      }
+    )
+  }
+
+  // useEffect{}
+
   return (
     <div className='parent'>
       <div className='sub-container'>
         <p>{item.staffName}</p>
-        <button>delete</button>
+        <button onClick={(e)=>{
+          deleteEmployee()
+        }}>delete</button>
       </div>
 
     </div>
